c094c62f7466d847dac25d719acc46808eff0a6f
Rafael Pita <rfpita.ti@gmail.com>
2026-04-29 14:23:01 -0300

    ui: Remove animacao de delay nos pacotes de precos

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -35,10 +35,6 @@ export function Pricing({ initialPlans }: { initialPlans: Plan[] }) {
           {plans.map((plan, index) => (
             <motion.div
               key={plan.id}
-              initial={{ opacity: 0, y: 30 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5, delay: index * 0.1 }}
-              viewport={{ once: true }}
               className="relative flex flex-col items-center"
             >
               {plan.badge && (
